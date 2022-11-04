834b6316638032ea13f51482472f4c8d9b3bbd60
Bob Pham <96148077+bob-pham@users.noreply.github.com>
2022-11-03 18:45:37 -0700

    Time: 3416 ms (33.29%), Space: 38.4 MB (54.66%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,43 +1,39 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        pairs = 0
-        center = 0
+        result = 0
         wordsF = {}
-        length = len(words)
         
         for word in words:
             if word not in wordsF:
                 wordsF[word] = 1
             else:
-                wordsF[word] += 1
+                wordsF[word] +=1
                 
-        print(wordsF)
-            
         for key in wordsF.keys():
             temp = key[1] + key[0]
             if temp == key:
-                if wordsF[key] % 2:
+                if wordsF[key] % 2: 
                     num = wordsF[key] - 1
                     wordsF[key] = 1
                 else:
                     num = wordsF[key]
                     wordsF[key] = 0
-                pairs += int(num / 2)
+                result += int(num * 2)
                 
             elif temp in wordsF and wordsF[temp]:
                 num = min(wordsF[key], wordsF[temp])
                 wordsF[key] -= num
                 wordsF[temp] -= num
                 
-                pairs += num
+                result += num * 4
                 
         for key in wordsF.keys():
             if wordsF[key] and key[0] == key[1]:
-                center = 2
+                result += 2
                 break
                 
     
-        return (4 * pairs) + center
+        return result
 
         
         
