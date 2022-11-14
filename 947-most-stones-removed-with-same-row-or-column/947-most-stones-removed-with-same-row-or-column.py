58078b1e26fe1160290e995b68011e027b32bada
Bob Pham <96148077+bob-pham@users.noreply.github.com>
2022-11-13 18:34:26 -0800

    Time: 1218 ms (33.07%), Space: 14.4 MB (99.27%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,26 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        x = {stones[0][0]}
+        y = {stones[0][1]}
+        
+        res = 0
+        cycles = 1
+        
+        while (stones):
+            not_found = []
+            for i in stones:
+                if i[0] in x or i[1] in y:
+                    x.add(i[0])
+                    y.add(i[1])
+                    res += 1
+                else:
+                    not_found.append(i)
+            
+            if not_found == stones:
+                cycles += 1
+                x = {not_found[0][0]}
+                y = {not_found[0][1]}
+                
+            stones = not_found
+        
+        return res - cycles
