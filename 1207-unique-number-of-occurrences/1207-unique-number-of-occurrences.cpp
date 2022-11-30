2353ef8e963bd634c060ec1e4c30abddeb947fc2
Bob Pham <96148077+bob-pham@users.noreply.github.com>
2022-11-29 17:55:54 -0800

    Time: 13 ms (11.03%), Space: 8.2 MB (20.75%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> counts;
+        unordered_set<int> occurances;
+        
+        for (int i : arr) {
+            counts[i]++;
+        }
+        
+        for (const auto i : counts) {
+            if (occurances.find(i.second) == occurances.end()) {
+                occurances.insert(i.second);
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
