877a2cdebd952ea864b75e71e26eb4b69857b8d6
Bob Pham <bobbaopham@gmail.com>
2025-02-06 10:10:41 -0800

    2025-02-06

--- 25.reverse-nodes-in-k-group.cpp
+++ 25.reverse-nodes-in-k-group.cpp
@@ -11,8 +11,77 @@
  */
 class Solution {
 public:
+
+    ListNode* reverseLinkedList(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+
+        ListNode * prev = head;
+        ListNode * curr = head->next;
+        head->next = nullptr;
+
+        while (curr != nullptr) {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    ListNode * splitKNodes(ListNode* head, int k) {
+        if (head == nullptr) {
+            return 0;
+        }
+
+        ListNode * curr = head;
+        int idx = 0;
+
+        while (curr != nullptr && idx < k - 1) {
+            curr = curr->next;
+            idx++;
+        }
+
+        if (idx < k - 1) {
+            return nullptr;
+        }
+
+        if (curr != nullptr) {
+            return curr;
+        }
+        return nullptr;
+    }
+
     ListNode* reverseKGroup(ListNode* head, int k) {
         
+        ListNode * curr = head;
+        ListNode * prev = nullptr;
+
+        while (curr != nullptr) {
+            ListNode * tail = splitKNodes(curr, k);
+            if (tail == nullptr) {
+                if (prev == nullptr) {
+                    return head;
+                }
+                prev->next = curr;
+                return head;
+            }
+            ListNode * next = tail->next;
+            tail->next = nullptr;
+            ListNode * temp = reverseLinkedList(curr);
+            if (prev == nullptr) {
+                head = temp;
+            } else {
+                prev->next = temp;
+            }
+
+            prev = curr;
+            curr = next;
+        }
+
+        return head;
     }
 };
 // @leet end
